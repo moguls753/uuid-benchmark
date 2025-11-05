bdb09bb277d5c5a52c66f84d6c85459d61f687c9
Eike <eike.rackwitz@mail.de>
2025-11-05 22:45:56 +0100

    uuid generation

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -1,26 +1,69 @@
-  package main
+package main
 
-  import (
-        "fmt"
-        "log"
-  )
+import (
+"fmt"
+"log"
+"math/rand"
+"time"
 
-  func main() {
-        fmt.Println("UUID Benchmark Tool")
-        fmt.Println("===================")
+"github.com/google/uuid"
+"github.com/oklog/ulid/v2"
+)
 
-        // Verify UUID library works
-        testUUIDs()
+func main() {
+fmt.Println("UUID Benchmark Tool")
+fmt.Println("===================")
+fmt.Println()
 
-        fmt.Println("\nReady for benchmarking.")
-  }
+numExamples := 5
 
-  func testUUIDs() {
-        // TODO: Add UUID generation tests
-        // - UUIDv1 (time-based)
-        // - UUIDv4 (random)
-        // - UUIDv7 (timestamp-sortable)
-        // - ULID (lexicographically sortable)
+fmt.Println("UUIDv1 (Time-based with MAC address):")
+fmt.Println("--------------------------------------")
+for i := 0; i < numExamples; i++ {
+	v1, err := uuid.NewUUID()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d. %s\n", i+1, v1.String())
+	time.Sleep(1 * time.Millisecond)
+}
+fmt.Println()
 
-        log.Println("UUID generation not yet implemented")
-  }
+fmt.Println("UUIDv4 (Random):")
+fmt.Println("----------------")
+for i := 0; i < numExamples; i++ {
+	v4 := uuid.New()
+	fmt.Printf("%d. %s\n", i+1, v4.String())
+}
+fmt.Println()
+
+fmt.Println("UUIDv7 (Timestamp-sortable):")
+fmt.Println("-----------------------------")
+for i := 0; i < numExamples; i++ {
+	v7, err := uuid.NewV7()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d. %s\n", i+1, v7.String())
+	time.Sleep(1 * time.Millisecond)
+}
+fmt.Println()
+
+fmt.Println("ULID (Lexicographically sortable):")
+fmt.Println("-----------------------------------")
+entropy := rand.New(rand.NewSource(time.Now().UnixNano()))
+for i := 0; i < numExamples; i++ {
+	ul := ulid.MustNew(ulid.Timestamp(time.Now()), entropy)
+	fmt.Printf("%d. %s\n", i+1, ul.String())
+	time.Sleep(1 * time.Millisecond)
+}
+fmt.Println()
+
+fmt.Println("INT8 / BIGSERIAL (Sequential integers - reference):")
+fmt.Println("----------------------------------------------------")
+var startID int64 = 1000000
+for i := 0; i < numExamples; i++ {
+	fmt.Printf("%d. %d (0x%016x)\n", i+1, startID+int64(i), startID+int64(i))
+}
+fmt.Println()
+}
